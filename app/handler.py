557e614330385149c757c006b9d0e8867515f7b3
sorashiro <sora95shiro@gmail.com>
2017-08-25 23:51:04 +0800

    try get fans msg

--- app/handler.py
+++ app/handler.py
@@ -4,43 +4,29 @@
 import hashlib
 import web
 
-'''
-CentOS need to add below code
-```
-import sys
-sys.path.append('/root/PycharmProjects/GitWeiXinPublic/WeiXinPublic')
-```
-'''
+import reply
+import receive
 
+import sys
+
+sys.path.append('/root/PycharmProjects/GitWeiXinPublic/WeiXinPublic')
 from secret import nj_token
 
 
 class Handle(object):
-    def GET(self):
+    def POST(self):
         try:
-            data = web.input()
-            if len(data) == 0:
-                return "hello, this is handle view"
-            signature = data.signature
-            timestamp = data.timestamp
-            nonce = data.nonce
-            echostr = data.echostr
-            token = nj_token.token  # 请按照公众平台官网\基本配置中信息填写
-
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
-            # print "handle/GET func: hashcode, signature: ", hashcode, signature
-            print "Here is something from fans:\n"
-            print data.Content
-            print "\n"
-            print data.FromUserName
-            if hashcode == signature:
-                return echostr
+            webData = web.data()
+            print "Handle Post webdata is ", webData  # 后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
             else:
-                print data.Content
-                return ""
-        except Exception, Argument:
-            return Argument
+                print "暂且不处理"
+                return "success"
+        except Exception, Argment:
+            return Argment
