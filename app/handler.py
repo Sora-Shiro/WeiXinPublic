61aef5942f1ab9b64f5c6cba4dbf1ff7f358398c
sorashiro <sora95shiro@gmail.com>
2017-08-26 00:34:06 +0800

    vote func try

--- app/handler.py
+++ app/handler.py
@@ -14,7 +14,9 @@ from secret import nj_token
 
 
 class Handle(object):
-    def POST(self):
+    global a, b, c, d, e
+
+    def POST(self, a=None):
         try:
             webData = web.data()
             print "Handle Post webdata is ", webData  # 后台打日志
@@ -22,9 +24,29 @@ class Handle(object):
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                if(recMsg.Content == 'Leo'):
+                if (recMsg.Content == 'Leo'):
                     content = u"Leo 超帅".encode('utf-8')
-                else :
+                elif (recMsg.Content == '1'):
+                    a += 1
+                    content = u"a当前得票数:" + a + u"b当前得票数:" + b + u"c当前得票数:" + c + u"d当前得票数:" + d + u"e当前得票数:" + e.encode(
+                        'utf-8')
+                elif (recMsg.Content == '2'):
+                    b += 1
+                    content = u"a当前得票数:" + a + u"b当前得票数:" + b + u"c当前得票数:" + c + u"d当前得票数:" + d + u"e当前得票数:" + e.encode(
+                        'utf-8')
+                elif (recMsg.Content == '3'):
+                    c += 1
+                    content = u"a当前得票数:" + a + u"b当前得票数:" + b + u"c当前得票数:" + c + u"d当前得票数:" + d + u"e当前得票数:" + e.encode(
+                        'utf-8')
+                elif (recMsg.Content == '4'):
+                    d += 1
+                    content = u"a当前得票数:" + a + u"b当前得票数:" + b + u"c当前得票数:" + c + u"d当前得票数:" + d + u"e当前得票数:" + e.encode(
+                        'utf-8')
+                elif (recMsg.Content == '5'):
+                    e += 1
+                    content = u"a当前得票数:" + a + u"b当前得票数:" + b + u"c当前得票数:" + c + u"d当前得票数:" + d + u"e当前得票数:" + e.encode(
+                        'utf-8')
+                else:
                     content = "Who are you"
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
                 return replyMsg.send()
--- app/receive.py
+++ app/receive.py
@@ -14,6 +14,7 @@ def parse_xml(web_data):
 
 class Msg(object):
     def __init__(self, xmlData):
+        self.Content = None
         self.ToUserName = xmlData.find('ToUserName').text
         self.FromUserName = xmlData.find('FromUserName').text
         self.CreateTime = xmlData.find('CreateTime').text
