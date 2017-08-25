b17da87b83f2b26dc6ec0dfc6dcacd18f8524e86
sorashiro <sora95shiro@gmail.com>
2017-08-25 14:00:58 +0800

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# filename: main.py
+import web
+
+urls = (
+    '/wx', 'Handle',
+)
+
+
+class Handle(object):
+    def GET(self):
+        return "Hello World"
+
+
+if __name__ == '__main__':
+    app = web.application(urls, globals())
+    app.run()
