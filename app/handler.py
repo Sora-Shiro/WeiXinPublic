8274a52ada43bb51678f4686089b0bceb0ad3065
sorashiro <sora95shiro@gmail.com>
2017-08-25 23:30:58 +0800

    try get fans msg

--- app/handler.py
+++ app/handler.py
@@ -3,7 +3,16 @@
 
 import hashlib
 import web
-import secret
+
+'''
+CentOS need to add below code
+```
+import sys
+sys.path.append('/root/PycharmProjects/GitWeiXinPublic/WeiXinPublic')
+```
+'''
+
+from secret import nj_token
 
 
 class Handle(object):
@@ -16,17 +25,22 @@ class Handle(object):
             timestamp = data.timestamp
             nonce = data.nonce
             echostr = data.echostr
-            token = secret.token  # 请按照公众平台官网\基本配置中信息填写
+            token = nj_token.token  # 请按照公众平台官网\基本配置中信息填写
 
             list = [token, timestamp, nonce]
             list.sort()
             sha1 = hashlib.sha1()
             map(sha1.update, list)
             hashcode = sha1.hexdigest()
-            print "handle/GET func: hashcode, signature: ", hashcode, signature
+            # print "handle/GET func: hashcode, signature: ", hashcode, signature
+            print "Here is something from fans:\n"
+            print data.Content
+            print "\n"
+            print data.FromUserName
             if hashcode == signature:
                 return echostr
             else:
+                print data.Content
                 return ""
         except Exception, Argument:
             return Argument
