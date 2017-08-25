a6d755333d0cca9ceaaeccede1a5d83dd74e5213
sorashiro <sora95shiro@gmail.com>
2017-08-25 21:00:23 +0800

    step 2

--- app/main.py
+++ app/main.py
@@ -2,15 +2,11 @@
 # filename: main.py
 import web
 
+from handler import Handle
+
 urls = (
     '/wx', 'Handle',
 )
-
-
-class Handle(object):
-    def GET(self):
-        return "Hello World!"
-
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
