4bee39f164552c576ed2007304d268630a5f166a
sorashiro <sora95shiro@gmail.com>
2017-08-26 13:55:18 +0800

    vote func try

--- app/handler.py
+++ app/handler.py
@@ -20,11 +20,11 @@ e_name = "e"
 
 nick_names = ["1", "2", "3", "4", "5"]
 
-vote_names = [a_name, b_name, c_name, d_name, e_name]
+real_names = [a_name, b_name, c_name, d_name, e_name]
 
-nick_to_real_map = dict(zip(nick_names, vote_names))
+nick_to_real_map = dict(zip(nick_names, real_names))
 
-name_vote_map = {
+real_to_vote_map = {
     a_name: 0,
     b_name: 0,
     c_name: 0,
@@ -33,13 +33,14 @@ name_vote_map = {
 }
 
 fans_number_set = set()
-admin_number_set = set(["tomatoes11", "farseerleo"])
+admin_number_set = set(["tomatoes11", "farseerleo",
+                        "o_hn0s0hhaGPwHfZ9mWo8RtnWA2A",
+                        "o_hn0szZLZ3nhY7m-9b9mSaqWRE0"])
 
 
 class Handle(object):
     def POST(self):
-        global a_name, b_name, c_name, d_name, e_name
-        global vote_names, name_vote_map, fans_number_set
+        global real_names, real_to_vote_map, fans_number_set, nick_to_real_map, admin_number_set
         try:
             webData = web.data()
             print "Handle Post webdata is ", webData  # 后台打日志
@@ -50,20 +51,59 @@ class Handle(object):
                 fromUser = recMsg.ToUserName
                 if (recMsg.Content == 'Leo'):
                     show_str = u"Leo 超帅"
+                '''
                 # revote enable 指令可以让发送者成为管理员
                 if recMsg.Content == "revote enable":
                     admin_number_set.add(toUser)
                     show_str = u"Revote Enable"
-                elif recMsg.Content == "rovote disable":
+                # revote disable 指令可以让发送者的管理员身份失效
+                elif recMsg.Content == "revote disable":
                     if toUser in admin_number_set:
                         admin_number_set.remove(toUser)
                         show_str = u"Rovote Disable"
+                '''
+                # 修改选手名字：alter [nickname] [realname]
+                if recMsg.Content.startswith("alter"):
+                    process_str = recMsg.Content.split()
+                    # 获取要更改的 nickname ，将其对应的 realname 改变
+                    nick = process_str[1]
+                    real = process_str[2]
+                    real_before = nick_to_real_map[nick]
+                    # 更新 数据
+                    real_name_list_index = real_names.index(real_before)
+                    real_names[real_name_list_index] = real
+                    nick_to_real_map = dict(zip(nick_names, real_names))
+                    vote = real_to_vote_map[real_before]
+                    del real_to_vote_map[real_before]
+                    real_to_vote_map[real] = vote
+                # 增加选手：add [nickname] [realname]
+                elif recMsg.Content.startswith("add"):
+                    process_str = recMsg.Content.split()
+                    # 获取要添加的 nickname 和对应的 realname
+                    nick = process_str[1]
+                    real = process_str[2]
+                    # 更新 数据
+                    nick_names.append(nick)
+                    real_names.append(real)
+                    nick_to_real_map = dict(zip(nick_names, real_names))
+                    vote = 0
+                    real_to_vote_map[real] = vote
+                # 修改票数：votec [nickname] [votes]
+                elif recMsg.Content.startswith("votec"):
+                    process_str = recMsg.Content.split()
+                    # 获取要修改票数的 nickname 和对应的 votes
+                    nick = process_str[1]
+                    votes = process_str[2]
+                    # 更新 数据
+                    real = nick_to_real_map[nick]
+                    real_to_vote_map[real] = votes
+                # 投票检验
                 elif recMsg.Content in nick_names:
                     # 检测是否为管理员
                     if toUser in admin_number_set:
                         nick_name = recMsg.Content
                         real_name = nick_to_real_map[nick_name]
-                        name_vote_map[real_name] += 1
+                        real_to_vote_map[real_name] += 1
                     else:
                         # 检测该id是否已经投票
                         if toUser in fans_number_set:
@@ -72,19 +112,19 @@ class Handle(object):
                             fans_number_set.add(toUser)
                             nick_name = recMsg.Content
                             real_name = nick_to_real_map[nick_name]
-                            name_vote_map[real_name] += 1
+                            real_to_vote_map[real_name] += 1
                     # 展示结果
                     for nick_name in nick_names:
                         real_name = nick_to_real_map[nick_name]
-                        show_str += u"%s号，%s得票数为：%d" % (nick_name, real_name, name_vote_map[real_name])
-                        if vote_names[-1] != real_name:
+                        show_str += u"%s号，%s得票数为：%d" % (nick_name, real_name, real_to_vote_map[real_name])
+                        if real_names[-1] != real_name:
                             show_str += "\n"
                 else:
                     # 展示结果
                     for nick_name in nick_names:
                         real_name = nick_to_real_map[nick_name]
-                        show_str += u"%s号，%s得票数为：%d" % (nick_name, real_name, name_vote_map[real_name])
-                        if vote_names[-1] != real_name:
+                        show_str += u"%s号，%s得票数为：%d" % (nick_name, real_name, real_to_vote_map[real_name])
+                        if real_names[-1] != real_name:
                             show_str += "\n"
                 # 格式化最终字符串
                 content = show_str.encode('utf-8')
@@ -95,3 +135,12 @@ class Handle(object):
                 return "success"
         except Exception, Argment:
             return Argment
+
+
+def save_votes():
+    save_votes_in_txt()
+    return ""
+
+
+def save_votes_in_txt():
+    return ""
