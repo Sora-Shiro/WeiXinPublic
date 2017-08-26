1eb9fb7fc1213241480ca3a99efd8cd8d613ae18
sorashiro <sora95shiro@gmail.com>
2017-08-26 16:39:18 +0800

    vote func try

--- app/handler.py
+++ app/handler.py
@@ -87,97 +87,89 @@ class Handle(object):
                         admin_number_set.remove(toUser)
                         show_str = u"Rovote Disable"
                 '''
-                if recMsg.Content in nick_names:
-                    # 检测是否为管理员
-                    if toUser in admin_number_set:
-                        # 修改选手名字：alter [nickname] [realname]
-                        if recMsg.Content.startswith("alter"):
-                            show_str = u"alter"
-                            process_str = recMsg.Content.split()
-                            # 获取要更改的 nickname ，将其对应的 realname 改变
-                            nick = process_str[1]
-                            real = process_str[2]
-                            real_before = nick_to_real_map[nick]
-                            # 更新 数据
-                            real_name_list_index = real_names.index(real_before)
-                            real_names[real_name_list_index] = real
-                            nick_to_real_map = dict(zip(nick_names, real_names))
-                            vote = real_to_vote_map[real_before]
-                            del real_to_vote_map[real_before]
-                            real_to_vote_map[real] = vote
-                        # 增加选手：add [nickname] [realname]
-                        elif recMsg.Content.startswith("add"):
-                            show_str = u"add"
-                            process_str = recMsg.Content.split()
-                            # 获取要添加的 nickname 和对应的 realname
-                            nick = process_str[1]
-                            real = u"%s" % process_str[2]
-                            # 更新 数据
-                            nick_names.append(nick)
-                            real_names.append(real)
-                            nick_to_real_map = dict(zip(nick_names, real_names))
-                            vote = 0
-                            real_to_vote_map[real] = vote
-                        # 删除选手：del [nickname]
-                        elif recMsg.Content.startswith("del"):
-                            show_str = u"del"
-                            process_str = recMsg.Content.split()
-                            # 获取要删除的 nickname
-                            nick = process_str[1]
-                            if nick in process_str:
-                                real = nick_to_real_map[nick]
-                                nick_names.remove(nick)
-                                real_names.remove(real)
-                                del nick_to_real_map[nick]
-                                del real_to_vote_map[real]
-                        # 修改票数：votec [nickname] [votes]
-                        elif recMsg.Content.startswith("votec"):
-                            show_str = u"votec"
-                            process_str = recMsg.Content.split()
-                            # 获取要修改票数的 nickname 和对应的 votes
-                            nick = process_str[1]
-                            votes = process_str[2]
-                            votes = int(votes)
-                            # 更新 数据
+                # 检测是否为管理员
+                if toUser in admin_number_set:
+                    # 修改选手名字：alter [nickname] [realname]
+                    if recMsg.Content.startswith("alter"):
+                        show_str = u"alter"
+                        process_str = recMsg.Content.split()
+                        # 获取要更改的 nickname ，将其对应的 realname 改变
+                        nick = process_str[1]
+                        real = process_str[2]
+                        real_before = nick_to_real_map[nick]
+                        # 更新 数据
+                        real_name_list_index = real_names.index(real_before)
+                        real_names[real_name_list_index] = real
+                        nick_to_real_map = dict(zip(nick_names, real_names))
+                        vote = real_to_vote_map[real_before]
+                        del real_to_vote_map[real_before]
+                        real_to_vote_map[real] = vote
+                    # 增加选手：add [nickname] [realname]
+                    elif recMsg.Content.startswith("add"):
+                        show_str = u"add"
+                        process_str = recMsg.Content.split()
+                        # 获取要添加的 nickname 和对应的 realname
+                        nick = process_str[1]
+                        real = u"%s" % process_str[2]
+                        # 更新 数据
+                        nick_names.append(nick)
+                        real_names.append(real)
+                        nick_to_real_map = dict(zip(nick_names, real_names))
+                        vote = 0
+                        real_to_vote_map[real] = vote
+                    # 删除选手：del [nickname]
+                    elif recMsg.Content.startswith("del"):
+                        show_str = u"del"
+                        process_str = recMsg.Content.split()
+                        # 获取要删除的 nickname
+                        nick = process_str[1]
+                        if nick in process_str:
                             real = nick_to_real_map[nick]
-                            real_to_vote_map[real] = votes
-                        # 复位票数和已投票粉丝：
-                        elif recMsg.Content.startswith("reset"):
-                            init_all_data()
-                        # 投票检验
-                        else:
-                            nick_name = recMsg.Content
-                            if nick_name in nick_names:
-                                real_name = nick_to_real_map[nick_name]
-                                real_to_vote_map[real_name] += 1
-                            else:
-                                show_str = u"没有这个号数的选手哦(⊙□⊙)"
+                            nick_names.remove(nick)
+                            real_names.remove(real)
+                            del nick_to_real_map[nick]
+                            del real_to_vote_map[real]
+                    # 修改票数：votec [nickname] [votes]
+                    elif recMsg.Content.startswith("votec"):
+                        show_str = u"votec"
+                        process_str = recMsg.Content.split()
+                        # 获取要修改票数的 nickname 和对应的 votes
+                        nick = process_str[1]
+                        votes = process_str[2]
+                        votes = int(votes)
+                        # 更新 数据
+                        real = nick_to_real_map[nick]
+                        real_to_vote_map[real] = votes
+                    # 复位票数和已投票粉丝：
+                    elif recMsg.Content.startswith("reset"):
+                        init_all_data()
+                    # 投票检验
                     else:
-                        # 检测该id是否已经投票
-                        if toUser in fans_number_set:
-                            show_str = u"您已经投过票，谢谢参与！(*´▽｀* )\n"
+                        nick_name = recMsg.Content
+                        if nick_name in nick_names:
+                            real_name = nick_to_real_map[nick_name]
+                            real_to_vote_map[real_name] += 1
                         else:
-                            fans_number_set.add(toUser)
-                            nick_name = recMsg.Content
-                            if nick_name in nick_names:
-                                real_name = nick_to_real_map[nick_name]
-                                real_to_vote_map[real_name] += 1
-                            else:
-                                show_str = u"没有这个号数的选手哦(⊙□⊙)"
-                    # 展示结果
-                    for nick_name in nick_names:
-                        real_name = nick_to_real_map[nick_name]
-                        show_str += u"%s号，%s得票数为：%d" % (nick_name, real_name, real_to_vote_map[real_name])
-                        if real_names[-1] != real_name:
-                            show_str += "\n"
+                            show_str = u"没有这个号数的选手哦(⊙□⊙)"
                 else:
-                    # 展示结果
-                    show_str = ""
-                    for nick_name in nick_names:
-                        real_name = nick_to_real_map[nick_name]
-                        show_str += u"%s号，%s得票数为：%d" % (nick_name, real_name, real_to_vote_map[real_name])
-                        if real_names[-1] != real_name:
-                            show_str += "\n"
+                    # 检测该非管理员人员是否已经投票
+                    if toUser in fans_number_set:
+                        show_str = u"您已经投过票，谢谢参与！(*´▽｀* )\n"
+                    # 投票检验
+                    else:
+                        fans_number_set.add(toUser)
+                        nick_name = recMsg.Content
+                        if nick_name in nick_names:
+                            real_name = nick_to_real_map[nick_name]
+                            real_to_vote_map[real_name] += 1
+                        else:
+                            show_str = u"没有这个号数的选手哦(⊙□⊙)"
+                # 展示结果
+                for nick_name in nick_names:
+                    real_name = nick_to_real_map[nick_name]
+                    show_str += u"%s号，%s得票数为：%d" % (nick_name, real_name, real_to_vote_map[real_name])
+                    if real_names[-1] != real_name:
+                        show_str += "\n"
                 # 格式化最终字符串
                 content = show_str.encode('utf-8')
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
