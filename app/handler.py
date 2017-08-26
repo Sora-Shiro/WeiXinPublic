a380ac1361c2574d0018b111545f0159a5f5384f
sorashiro <sora95shiro@gmail.com>
2017-08-26 14:27:35 +0800

    vote func try

--- app/handler.py
+++ app/handler.py
@@ -134,10 +134,10 @@ class Handle(object):
                     for nick_name in nick_names:
                         real_name = nick_to_real_map[nick_name]
                         print real_name
-                        show_str += u"%s号，%s得票数为：%d" % (nick_name, (real_name + u"").encode('utf-8'), real_to_vote_map[real_name])
-                        print "ok?"
-                        print real_name + u""
-                        print "ye"
+                        print "ahh"
+                        print real_to_vote_map[real_name]
+                        print "pre"
+                        show_str += nick_name + " : " + real_name + " : " + real_to_vote_map[real_name]
                         if real_names[-1] != real_name:
                             show_str += "\n"
                 # 格式化最终字符串
