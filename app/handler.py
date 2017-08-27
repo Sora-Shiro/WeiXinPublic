8eb0876ec8481690a9b21952cc04f1a48bd1f5e9
sorashiro <sora95shiro@gmail.com>
2017-08-27 13:20:46 +0800

    robot test

--- app/handler.py
+++ app/handler.py
@@ -3,6 +3,7 @@
 
 import hashlib
 import threading
+import urllib2
 
 import web
 import codecs
@@ -11,6 +12,9 @@ import reply
 import receive
 
 import sys
+
+from app import talk_api
+
 sys.path.append('/root/PycharmProjects/GitWeiXinPublic/WeiXinPublic')
 from secret import nj_token
 
@@ -41,15 +45,16 @@ class Handle(object):
                 strShow = u""
                 strToUser = recMsg.FromUserName
                 strFromUser = recMsg.ToUserName
-                if (recMsg.Content == 'Leo'):
+                strUserWord = recMsg.Content
+                if (strUserWord == 'Leo'):
                     strShow = u"Leo 超帅"
                 '''
                 # revote boolEnable 指令可以让发送者成为管理员
-                if recMsg.Content == "revote boolEnable":
+                if strUserWord == "revote boolEnable":
                     admin_number_set.add(strToUser)
                     strShow = u"Revote Enable"
                 # revote disable 指令可以让发送者的管理员身份失效
-                elif recMsg.Content == "revote disable":
+                elif strUserWord == "revote disable":
                     if strToUser in admin_number_set:
                         admin_number_set.remove(strToUser)
                         strShow = u"Rovote Disable"
@@ -57,9 +62,9 @@ class Handle(object):
                 # 检测是否为管理员
                 if strToUser in g_stAdminNumber:
                     # 修改选手名字：alter [strOrderNum] [strName]
-                    if recMsg.Content.startswith("alter"):
+                    if strUserWord.startswith("alter"):
                         strShow = u"alter\n"
-                        listProcess = recMsg.Content.split()
+                        listProcess = strUserWord.split()
                         # 获取要更改的 player ，将其对应的 strName 改变
                         strOrderNum = listProcess[1]
                         strName = listProcess[2]
@@ -67,9 +72,9 @@ class Handle(object):
                         # 更新 数据
                         player.strName = strName
                     # 增加选手：add [strOrderNum] [strName]
-                    elif recMsg.Content.startswith("add"):
+                    elif strUserWord.startswith("add"):
                         strShow = u"add\n"
-                        listProcess = recMsg.Content.split()
+                        listProcess = strUserWord.split()
                         # 获取要添加的 player 和对应的 strOrderNum
                         strOrderNum = listProcess[1]
                         strName = listProcess[2]
@@ -84,9 +89,9 @@ class Handle(object):
                         else:
                             strShow = u"该号数已经存在"
                     # 删除选手：del [strOrderNum]
-                    elif recMsg.Content.startswith("del"):
+                    elif strUserWord.startswith("del"):
                         strShow = u"del\n"
-                        listProcess = recMsg.Content.split()
+                        listProcess = strUserWord.split()
                         # 获取要删除的 nickname
                         strOrderNum = listProcess[1]
                         if strOrderNum in g_listOrderNums:
@@ -95,9 +100,9 @@ class Handle(object):
                             g_listPlayers.remove(strName)
                             del g_dictOrderToPlayer[strOrderNum]
                     # 修改票数：votec [order_name] [intVotes]
-                    elif recMsg.Content.startswith("votec"):
+                    elif strUserWord.startswith("votec"):
                         strShow = u"votec\n"
-                        listProcess = recMsg.Content.split()
+                        listProcess = strUserWord.split()
                         # 获取要修改票数的 nickname 和对应的 intVotes
                         strOrderNum = listProcess[1]
                         intVotes = int(listProcess[2])
@@ -105,41 +110,58 @@ class Handle(object):
                         player = g_dictOrderToPlayer[strOrderNum]
                         player.intVotes = intVotes
                     # 复位票数和已投票粉丝：
-                    elif recMsg.Content.startswith("reset"):
+                    elif strUserWord.startswith("reset"):
                         init_all_data()
                     # 立即存储数据
-                    elif recMsg.Content.startswith("saveall"):
+                    elif strUserWord.startswith("saveall"):
                         save_data()
                     # 投票检验
-                    else:
-                        strOrderNum = recMsg.Content
+                    elif strUserWord.startswith("toupiao"):
+                        strOrderNum = strUserWord[7:]
                         if strOrderNum in g_listOrderNums:
                             player = g_dictOrderToPlayer[strOrderNum]
                             player.intVotes += 1
                             strShow = u"已经成功投给%s号选手%s！\n" % (strOrderNum, player.strName)
                         else:
                             strShow = u"没有这个号数的选手哦(⊙□⊙)\n"
+                        # 展示投票结果
+                        for strOrderNum in g_listOrderNums:
+                            player = g_dictOrderToPlayer[strOrderNum]
+                            strShow += u"%s号，%s得票数为：%d" % (strOrderNum, player.strName, player.intVotes)
+                            if g_listOrderNums[-1] != strOrderNum:
+                                strShow += "\n"
+                    # 其他文本交给机器人
+                    else:
+                        strShow = robot_talk(strUserWord, strFromUser[0:15])
+                # 非管理员处理
                 else:
-                    # 检测该非管理员人员是否已经投票
-                    if strToUser in g_stFansNumber:
-                        strShow = u"您已经投过票，谢谢参与！(*´▽｀* )\n"
-                    # 投票检验
-                    else:
-                        strOrderNum = recMsg.Content
-                        if strOrderNum in g_listOrderNums:
-                            g_stFansNumber.add(strToUser)
-                            player = g_dictOrderToPlayer[strOrderNum]
-                            player.intVotes += 1
-                            strShow = u"已经成功投给%s号选手%s！\n" % (strOrderNum, player.strName)
+                    # 投票处理
+                    if strUserWord.startswith("toupiao"):
+                        # 检测该非管理员人员是否已经投票
+                        if strToUser in g_stFansNumber:
+                            strShow = u"您已经投过票，谢谢参与！(*´▽｀* )\n"
+                        # 投票检验
                         else:
-                            strShow = u"没有这个号数的选手哦(⊙□⊙)\n"
-                # 展示结果
-                for strOrderNum in g_listOrderNums:
-                    player = g_dictOrderToPlayer[strOrderNum]
-                    strShow += u"%s号，%s得票数为：%d" % (strOrderNum, player.strName, player.intVotes)
-                    if g_listOrderNums[-1] != strOrderNum:
-                        strShow += "\n"
+                            strOrderNum = strUserWord[7:]
+                            if strOrderNum in g_listOrderNums:
+                                g_stFansNumber.add(strToUser)
+                                player = g_dictOrderToPlayer[strOrderNum]
+                                player.intVotes += 1
+                                strShow = u"已经成功投给%s号选手%s！\n" % (strOrderNum, player.strName)
+                            else:
+                                strShow = u"没有这个号数的选手哦(⊙□⊙)\n"
+                        # 展示投票结果
+                        for strOrderNum in g_listOrderNums:
+                            player = g_dictOrderToPlayer[strOrderNum]
+                            strShow += u"%s号，%s得票数为：%d" % (strOrderNum, player.strName, player.intVotes)
+                            if g_listOrderNums[-1] != strOrderNum:
+                                strShow += "\n"
+                    # 其他文本交给机器人
+                    else:
+                        strShow = robot_talk(strUserWord, strFromUser[0:15])
                 # 格式化最终字符串
+                if len(strShow) == 0:
+                    return ""
                 strContent = strShow.encode('utf-8')
                 replyMsg = reply.TextMsg(strToUser, strFromUser, strContent)
                 # save_data()
@@ -148,13 +170,20 @@ class Handle(object):
                 if recMsg.Event == 'CLICK':
                     if recMsg.Eventkey == 'mpGuide':
                         content = u"编写中，尚未完成".encode('utf-8')
-                        replyMsg = reply.TextMsg(toUser, fromUser, content)
-                        return replyMsg.send()
+                        # replyMsg = reply.TextMsg(toUser, fromUser, content)
+                        # return replyMsg.send()
             else:
                 print "暂且不处理"
-                return "success"
+                return ""
         except Exception, Argment:
-            return Argment
+            # return Argment
+            return ""
+
+
+def robot_talk(strUserWord, strUserId):
+    msg = talk_api.talk(strUserWord, strUserId)
+    return msg
+
 
 
 def init_all_data(strOrderNum=None, players=None, dictOrderToPlayer=None):
---
+++ app/talk_api.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# filename: talk_api.py
+
+import json
+import requests
+import sys
+
+sys.path.append('/root/PycharmProjects/GitWeiXinPublic/WeiXinPublic')
+from secret.nj_token import tuling_api_key
+
+s = requests.session()
+
+def talk(content, userid):
+    url = 'http://www.tuling123.com/openapi/api'
+    da = {"key": tuling_api_key, "info": content, "userid": userid}
+    data = json.dumps(da)
+    r = s.post(url, data=data)
+    j = eval(r.text)
+    code = j['code']
+    if code == 100000:
+        recontent = j['text']
+    elif code == 200000:
+        recontent = j['text'] + j['url']
+    elif code == 302000:
+        recontent = j['text'] + j['list'][0]['info'] + j['list'][0]['detailurl']
+    elif code == 308000:
+        recontent = j['text'] + j['list'][0]['info'] + j['list'][0]['detailurl']
+    else:
+        recontent = '这货还没学会怎么回复这句话'
+    return recontent
