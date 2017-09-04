51198481b2131b29d89ec73e7de9fc3c62970bc8
sorashiro <sora95shiro@gmail.com>
2017-09-04 21:48:37 +0800

    token

--- app/handler.py
+++ app/handler.py
@@ -32,6 +32,31 @@ g_stAdminNumber = set(g_listAdmins)
 
 
 class Handle(object):
+    def GET(self):
+        try:
+            data = web.input()
+            if len(data) == 0:
+                return "hello, this is handle view"
+            signature = data.signature
+            timestamp = data.timestamp
+            nonce = data.nonce
+            echostr = data.echostr
+            token = nj_token.token #请按照公众平台官网\基本配置中信息填写
+
+            list = [token, timestamp, nonce]
+            list.sort()
+            sha1 = hashlib.sha1()
+            map(sha1.update, list)
+            hashcode = sha1.hexdigest()
+            print "handle/GET func: hashcode, signature: ", hashcode, signature
+            if hashcode == signature:
+                return echostr
+            else:
+                return ""
+        except Exception, Argument:
+            return Argument
+
+
     def POST(self):
         global g_listPlayers, g_stFansNumber, g_dictOrderToPlayer, g_stAdminNumber
         try:
